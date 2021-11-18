5cf3f00fc7301733ca86c141e942328103101cf8
Tuna Karacan <46001228+RealIstros@users.noreply.github.com>
2021-11-18 09:16:12 +0300

    Added naive edge detetction
    Added naive edge detetction and fixed some problems with Sobel.

--- Source/Functions.py
+++ Source/Functions.py
@@ -39,7 +39,7 @@ def bilateral_blur(image, kernel_size, sigma_color=75, sigma_space=75):
 
 
 def de_blur(image):
-    de_blur_kernel = np.array([[0, -1, 0], [-1, 6, -1], [0, -1, 0]])
+    de_blur_kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
     manipulated_image = cv2.filter2D(image, -1, de_blur_kernel)
     return manipulated_image
 
@@ -94,7 +94,7 @@ def change_contrast_and_brightness(image, alpha=1, beta=0, gamma=1):
     return manipulated_image
 
 
-def gaussian_noise(image, mean=0, var=1):
+def gaussian_noise(image, mean=0, var=0.01):
     noise_image = skimage.util.random_noise(image=image, mode='gaussian', mean=mean, var=var)
     manipulated_image = np.array(255 * noise_image, dtype='uint8')
     return manipulated_image
@@ -112,15 +112,22 @@ def poisson_noise(image):
     return manipulated_image
 
 
-def speckle_noise(image, mean=0, var=1):
+def speckle_noise(image, mean=0, var=0.01):
     noise_image = skimage.util.random_noise(image=image, mode='speckle', mean=mean, var=var)
     manipulated_image = np.array(255 * noise_image, dtype='uint8')
     return manipulated_image
 
 
+def naive_edge_detect(image):
+    grayscaled_image = grayscale_image(image)
+    detection_kernel = np.array([[-1, -1, -1], [-1, 7, -1], [-1, -1, -1]])
+    manipulated_image = cv2.filter2D(grayscaled_image, -1, detection_kernel)
+    return manipulated_image
+
+
 def sobel_edge_detect(image, kernel_size, dx=0, dy=0):
     grayscaled_image = grayscale_image(image)
-    manipulated_image = cv2.Sobel(grayscaled_image, ksize=kernel_size, dx=dx, dy=dy, ddepth=cv2.CV_64F)
+    manipulated_image = cv2.Sobel(grayscaled_image, ksize=kernel_size, dx=dx, dy=dy, ddepth=cv2.CV_8U)
     return manipulated_image
 
 
